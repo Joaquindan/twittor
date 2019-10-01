4e1734d042d063e97308ce64cc3cdc279816b944
Joaquin Daneri <joaquindaneri@gmail.com>
2019-09-30 21:02:58 -0300

    modificaciones ios

--- sw.js
+++ sw.js
@@ -1,19 +1,7 @@
-// import
-// var url = window.location.href;
-// var swUtilsLocation = '/twittor/sw.js';
-
-// if (navigator.serviceWorker) {
-//     if (url.includes('localhost')) {
-//         swLocation = '/sw.js';
-//     }
-//     navigator.serviceWorker.register(swLocation);
-// }
-
-// importScripts('https://joaquindan.github.io/twittor/js/sw-utils.js');
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v5';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -64,7 +52,10 @@ self.addEventListener('activate', e => {
                             keys.forEach(key => {
                                 if (key != STATIC_CACHE && key.includes('static')) {
                                     return caches.delete(key);
-                                }
+                                };
+                                if (key != DYNAMIC_CACHE && key.includes('dynamic')) {
+                                    return caches.delete(key);
+                                };
                             });
                         });
     
